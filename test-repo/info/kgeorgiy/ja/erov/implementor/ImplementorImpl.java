298dc2c04c78a3a0411461c396b1b88d5d72cebe
iamerove <erove@inbox.ru>
2022-06-21 23:38:09 +0300

    - temporary files

--- test-repo/info/kgeorgiy/ja/erov/implementor/ImplementorImpl.java
+++
@@ -1,8 +0,0 @@
-package info.kgeorgiy.ja.erov.implementor;
-
-public class ImplementorImpl extends info.kgeorgiy.ja.erov.implementor.Implementor {
-
-	public ImplementorImpl() {
-		super();
-	}
-}
--- test-repo/info/kgeorgiy/ja/erov/implementor/MainImpl.java
+++
@@ -1,8 +0,0 @@
-package info.kgeorgiy.ja.erov.implementor;
-
-public class MainImpl extends info.kgeorgiy.ja.erov.implementor.Main {
-
-	MainImpl() {
-		super();
-	}
-}
